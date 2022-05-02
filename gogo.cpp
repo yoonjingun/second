76058b2a99fc81634b7bc31f4ad753242c5719c2
dbswlsrjs <wlsrjsdlaekd@naver.com>
2022-05-02 09:33:33 +0900

    sasd

---
+++ gogo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+
+	int money = 100;
+
+	std::cout << money + 3 << std::endl;
+	std::cout << money - 3 << std::endl;
+	std::cout << money * 3 << std::endl;
+	std::cout << money / 3 << std::endl;
+
+	float R = 0.1f;
+	std::cout << R + 0.3f << std::endl;
+	std::cout << R - 0.3f << std::endl;
+	std::cout << R / 0.3f << std::endl;
+	std::cout << R * 0.3f << std::endl;
+
+
+
+
+
+	std:: cout << "¾È³çÇÏ¼¼¿ä" << std:: endl;
+
+	return 0;
+
+}
